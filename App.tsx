38c15346b1592118d12beb85a877d0eecd7fed07
JGGrande <joaogabriel7303@gmail.com>
2026-03-11 19:07:17 -0300

    First commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { ScrollView } from "react-native";
+import ProfileScreen from "./app/screens/profile";
+import HomeScreen from "./app/screens/home";
+
+export default function App() {
+  return (
+    <>
+      {/* <ProfileScreen /> */}
+      <HomeScreen />
+    </>
+  );
+}
